bb39e81693de0f123307b6d9c9f9912598ff1669
mayahesham <96448594+mayahesham@users.noreply.github.com>
2024-04-06 17:16:54 +0200

    Update form.js

--- javascript/form.js
+++ javascript/form.js
@@ -1,79 +1,125 @@
 document.addEventListener("DOMContentLoaded", function () {
     const form = document.getElementById("registrationForm");
 
-    form.addEventListener("submit", function (event) {
-        event.preventDefault(); 
-
-        const name = document.getElementById("name").value
-        const userName = document.getElementById("user-name").value
-        const email = document.getElementById("email").value
-        const phoneNumber = document.getElementById("phone-number").value
-        const address = document.getElementById("address").value
-        const password = document.getElementById("password").value
-        const confirmPassword = document.getElementById("confirm_password").value
-        const birthDate = document.getElementById("Birth").value
-        const imageInput = document.getElementById("imageInput");
-
-        let isValid = true; 
-
-        function displayError(inputField, message) {
-            const parent = inputField.parentElement;
-            const errorElement = parent.querySelector(".error-message");
-            if (errorElement) {
-                errorElement.textContent = message;
-            } else {
-                const errorMessage = document.createElement("span");
-                errorMessage.className = "error-message";
-                errorMessage.textContent = message;
-                errorMessage.style.color = "red"; 
-                errorMessage.style.display = "block";
-                parent.appendChild(errorMessage);
-            }
-            isValid = false;
-        }
-        
-        if (!name || !userName || !email || !phoneNumber || !address || !password || !confirmPassword || !birthDate||!imageInput) {
-            showAlert("All fields are required.",'danger');
-        }
-
-        if (name.split(" ").length < 2 || !/^[a-zA-Z\s]+$/.test(name)) {
-            displayError(document.getElementById("name"), "write full name");
-        }
-
-        
-        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-       if (!emailRegex.test(email)) {
-            displayError(document.getElementById("email"), "Invalid email address.");
-        }
-        const today = new Date();
-        const birthDateTime = new Date(birthDate);
-        if (birthDateTime >= today) {
-            displayError(document.getElementById("Birth"), "inavalid Date of birth ");
-        }
-
-        const passwordRegex = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,}$/;
-        if (!passwordRegex.test(password)) {
-            displayError(document.getElementById("password"), "Password must be at least 8 characters long and contain at least one number and one special character.");
-        }
-
-        if (confirmPassword !== password) {
-            displayError(document.getElementById("confirm_password"), "Passwords do not match.");
-        }
-
-        if (isValid) {
-            // const successMessage = document.createElement("div");
-            // successMessage.className = "alert alert-success";
-            // successMessage.textContent = "User registered successfully.";
-            // form.parentNode.insertBefore(successMessage, form.nextSibling);
-            form.submit();
-        }
+    const fieldsID = ["name", "user-name", "email", "phone-number", "address", "password", "confirm_password", "Birth"];
+    fieldsID.forEach(fieldID => {
+        const field = document.getElementById(fieldID);
+        field.addEventListener("blur", function () {
+            validation(field);
+        });
     });
+
+    form.addEventListener("submit", function (event) {
+        event.preventDefault();
+
+        fieldsID.forEach(fieldID => {
+            const field = document.getElementById(fieldID);
+            validation(field);
+        });
+
+        const invalidFields = form.querySelectorAll(".error-message");
+        if (invalidFields.length > 0) {
+            showAlert("Invalid input.", 'danger');
+            return;
+        }
+
+        const formData = new FormData(form);
+        const xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function () {
+                if (xhr.status === 200 && xhr.readyState == 4) {
+                    const responseText = xhr.responseText;
+                    const alertRegex = /showAlert\('(.+)', '(.+)'\);/;
+                    const match = alertRegex.exec(responseText);
+                    if (match) {
+                        const message = match[1];
+                        const type = match[2];
+                        showAlert(message, type);
+                        if (message.trim().toLowerCase() === "user registered successfully.") {
+                            form.reset();
+                        }
+                    } else {
+                        showAlert("server error", 'danger');
+                    }
+                } 
+        };
+        xhr.open("POST", "upload.php", true);
+        xhr.send(formData);
+    });
+    
 });
+function validation(inputField) {
+    const fieldid = inputField.id;
+    const value = inputField.value.trim();
+    const parent = inputField.parentElement;
+    const errorElement = parent.querySelector(".error-message");
 
+    if (errorElement) {
+        errorElement.remove();
+    }
+    if (value === "") {
+        displayError(inputField, "This field is required.");
+        return false;
+    }
 
-
-
-
+    switch (fieldid) {
+        case "name":
+            if (value.split(" ").length < 2 || !/^[a-zA-Z\s]+$/.test(value)) {
+                displayError(inputField, "Write full name.");
+            }
+            break;
+        case "email":
+            const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+            if (!emailRegex.test(value)) {
+                displayError(inputField, "Invalid email address.");
+            }
+            break;
+        case "Birth":
+            const today = new Date();
+            const birthDateTime = new Date(value);
+            if (birthDateTime >= today) {
+                displayError(inputField, "Invalid Date of Birth.");
+            }
+            break;
+        case "password":
+            const passwordRegex = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,}$/;
+            if (!passwordRegex.test(value)) {
+                displayError(inputField, "Password must be at least 8 characters long and contain at least one number and one special character.");
+            }
+            break;
+        case "confirm_password":
+            const password = document.getElementById("password").value.trim();
+            if (value !== password) {
+                displayError(inputField, "Passwords do not match.");
+            }
+            break;
+        default:
+            break;
+    }
+}
+function displayError(inputField, message) {
+    const parent = inputField.parentElement;
+    const errorElement = parent.querySelector(".error-message");
+    if (errorElement) {
+        errorElement.textContent = message;
+    } else {
+        const errorMessage = document.createElement("span");
+        errorMessage.className = "error-message";
+        errorMessage.textContent = message;
+        errorMessage.style.color = "red"; 
+        errorMessage.style.display = "block";
+        parent.appendChild(errorMessage);
+    }
+}
+function showAlert(message, type) {
+    var alertDiv = document.createElement("div");
+    alertDiv.className = "alert alert-" + type;
+    alertDiv.setAttribute("role", "alert");
+    alertDiv.innerHTML = message;
+    document.getElementById("alertContainer").appendChild(alertDiv);
+    setTimeout(function () {
+        alertDiv.remove();
+    }, 5000);
+}
 function handleImageUpload(event) {
     const file = event.target.files[0];
     const validFormats = ['image/jpeg', 'image/png', 'image/gif'];
@@ -94,17 +140,4 @@ function handleImageUpload(event) {
         }
         reader.readAsDataURL(file);
     }
-}
-
-
-
-function showAlert(message, type) {
-    var alertDiv = document.createElement("div");
-    alertDiv.className = "alert alert-" + type;
-    alertDiv.setAttribute("role", "alert");
-    alertDiv.innerHTML = message;
-    document.getElementById("alertContainer").appendChild(alertDiv);
-    setTimeout(function () {
-        alertDiv.remove();
-    }, 3000);
 }
